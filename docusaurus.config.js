181f86adece69c12b90cd56343f515849ba302ef
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-06-21 11:42:00 -0700

    baseUrl trailing slash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Flashbake',
   tagline: 'A private mempool for Tezos',
   url: 'https://flashbake.xyz',
-  baseUrl: '/documentation',
+  baseUrl: '/documentation/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
