8d17ed150fb332e466e185971fd3c050f81fceef
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-07-15 20:56:29 -0700

    fix edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,10 +34,8 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/flashbake/',
+            'https://github.com/flashbake/documentation',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
