7ab6bd5987108495a39eadda985f4382c198b24d
Nicolas Ochem <nicolas.ochem@gmail.com>
2022-08-02 16:53:19 -0700

    add statistics to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
 
@@ -27,7 +26,34 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <section className={styles.features}>
+          <div className="container text--center">
+            <div className="row">
+              <p style={{ marginTop: 4 + 'em' }}>
+                <h1>Flashbake is available on Ghostnet! Read more on <a href="https://medium.com">FIXME the Medium post FIXME</a>.</h1>
+              </p>
+              <p style={{ marginBottom: 4 + 'em' }}>
+                <h2>Use the following endpoint to send a Flashbake transaction: <code>--endpoint https://ghostnet.relay.flashbake.xyz</code>.</h2>
+              </p>
+            </div>
+          </div>
+          <div className="container text--center">
+            <div className="row">
+              <div className="col">
+                <h3>Number of Flashbake transactions</h3>
+                <img src="https://gorgon.service.eu.newrelic.com/image/a94ba54e-f622-4d9e-b70a-47eb9dbf93e2?type=billboard" height="280px" />
+              </div>
+              <div className="col">
+                <h3>Number of Flashbakers on Ghostnet</h3>
+                <img src="https://gorgon.service.eu.newrelic.com/image/c732b910-d9ae-4c6d-a179-3b34507d891f?type=billboard" height="280px" />
+              </div>
+              <div className="col">
+                <h3>Average inclusion time</h3>
+                <img src="https://gorgon.service.eu.newrelic.com/image/95ddb8c4-eb3a-49a8-8b3e-aee1df659550?type=billboard" height="280px" />
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </Layout>
   );
